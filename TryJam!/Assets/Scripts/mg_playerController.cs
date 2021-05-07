c3f5d31c4cb67a88b7e4fc056258797fdb0d6f2c
ShimaCoding <shimacoding@gmail.com>
2021-05-06 21:21:52 -0400

    playerController
    playerController con movimiento
    enemyController vacio

---
+++ TryJam!/Assets/Scripts/mg_playerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mg_playerController : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+    public Rigidbody rb;
+    Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetAxis();
+    }
+
+    private void GetAxis()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
