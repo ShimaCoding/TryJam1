439796e0b47a5eeab9fcaf9e594b6a7a0ce91b5d
ShimaCoding <shimacoding@gmail.com>
2021-05-07 03:25:37 -0400

    Raycasts

--- TryJam!/Assets/Scripts/Enemies/en_enemyControllerPig.cs
+++ TryJam!/Assets/Scripts/Enemies/en_enemyControllerPig.cs
@@ -17,22 +17,28 @@ public class en_enemyControllerPig : MonoBehaviour
     {
 
         RaycastHit hit;
+        Vector3 lastPosition;
         Ray landingRay = new Ray(transform.position, Vector3.forward);
         if (Physics.Raycast(landingRay, out hit, dist))
         {
             if (hit.collider.tag == "Player")
             {
-                Vector3 lastPosition = hit.collider.transform.position;
+                //Obtengo la posicion del player
+                lastPosition = hit.collider.transform.position;
                 //Insertar Logica de ejecucion de la carga del chancho
                 Debug.Log("Realizar Carga!!!!");
                 MoveTo(lastPosition);
             }
+           
         }
-
+        
     }
     private void MoveTo(Vector3 position)
     {
         if (Vector3.Distance(transform.position, position) >= 2)
+        {
             rb.MovePosition(Vector3.Lerp(transform.position, position, speedMove * Time.fixedDeltaTime));
+        }
+
     }
 }
--- TryJam!/Assets/Scripts/Player/pj_playerController.cs
+++ TryJam!/Assets/Scripts/Player/pj_playerController.cs
@@ -7,6 +7,12 @@ public class pj_playerController : MonoBehaviour
     public float moveSpeed = 7f;
     public Rigidbody rb;
     Vector3 movement;
+    public float attackRange = 3f;
+    private Vector3 mousePos;
+    Vector3 clickPosition;
+    public LayerMask layer;
+
+
 
     void Start()
     {
@@ -15,7 +21,10 @@ public class pj_playerController : MonoBehaviour
 
     void Update()
     {
+
         GetAxis();
+
+
     }
 
     private void GetAxis()
@@ -27,6 +36,49 @@ public class pj_playerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //RaycastHit hit;
+        //Ray ray;
+        //ray = new Ray(transform.position, movement);
+        //if (Physics.Raycast(ray, out hit, attackRange))
+        //{
+
+        //    if (hit.collider.tag == "Enemy")
+        //    {
+        //        print(hit.collider.name);
+        //        if (Input.GetKeyDown(KeyCode.Space))
+        //        {
+        //            hit.transform.GetComponent<mg_rewind>().RewindStart();
+
+        //        }
+
+        //    }
+
+        //}
+
+        RaycastHit hit;
+        Ray rey = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(rey, out hit, Mathf.Infinity, layer))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                print(hit.collider.name);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    hit.transform.GetComponent<mg_rewind>().RewindStart();
+
+                }
+
+            }
+            clickPosition = hit.point;
+            
+        }
+        Debug.DrawRay(transform.position, (clickPosition - transform.position).normalized * attackRange, Color.red);
+        Debug.Log(clickPosition);
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+
+
     }
 }
