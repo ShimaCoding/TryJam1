23819615449a48d26cab4db79fe871d44b71ea14
Plumbio <felipe.b@live.cl>
2021-05-06 21:25:04 -0400

    script de grabado y rebobinación de movimiento y reorganización de scripts

---
+++ TryJam!/Assets/Scripts/Management/mg_rewind.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindState {
+    public Vector2 pos;
+    public Vector3 rot;
+    public Vector3 scale;
+}
+
+public class mg_rewind : MonoBehaviour {
+
+    List<RewindState> rewindStates;
+    List<RewindState> newRewindStates;
+    public float timeLimit = 5f;
+    bool rewinding;
+
+    void FixedUpdate() {
+        RewindState state = new RewindState { pos = transform.position, rot = transform.eulerAngles, scale = transform.localScale };
+        rewindStates.Add(state);
+        if (timeLimit <= 0)
+            rewindStates.RemoveAt(0);
+        timeLimit -= Time.deltaTime;
+        if (rewindStates.Count == 0)
+            timeLimit = 5;
+        if (rewinding) {
+            transform.position = newRewindStates[newRewindStates.Count - 1].pos;
+            transform.eulerAngles = newRewindStates[newRewindStates.Count - 1].rot;
+            transform.localScale = newRewindStates[newRewindStates.Count - 1].scale;
+            newRewindStates.RemoveAt(newRewindStates.Count - 1);
+        }
+    }
+    public void Rewind () {
+        newRewindStates = new List<RewindState>(rewindStates);
+        rewinding = true;
+    }
+    public void Stop () {
+        rewinding = false;
+    }
+}
