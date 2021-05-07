495d072c1305aebaecce790a321c4d6186dbff1b
Plumbio <felipe.b@live.cl>
2021-05-07 02:32:21 -0400

    script mg_rewind posíblemente completado y modelo de chancho añadido

--- TryJam!/Assets/Scripts/Management/mg_rewind.cs
+++ TryJam!/Assets/Scripts/Management/mg_rewind.cs
@@ -6,6 +6,7 @@ public class RewindState {
     public Vector3 pos;
     public Vector3 rot;
     public Vector3 scale;
+    public string action;//para indicar el nombre de la función y posibles variables, para hacerlo, se escribe primero el nombre de la función y luego las variables, separándolas usando ; y sin dejar ningún espacio, utilizar solo números, puntos y usar comas para separar entre dígitos de la mmisma variable (como un Vector2), no usar paréntesis ni niuna otra tontera.
 }
 
 public class mg_rewind : MonoBehaviour {
@@ -13,6 +14,7 @@ public class mg_rewind : MonoBehaviour {
     public float timeLimit = 3f;//límite de tiempo para empezar a borrar elementos
     bool rewinding;//determina si se está rebobinando
     bool replaying;//determmina si se están reproduciendo los estados rebobinados
+    string action = null;
 
     public GameObject rewindableShadowObj;//corresponde a la sombra invocada que rebobinará las acciones del objeto principal
     public GameObject mainBodyObj;//para extraer información del Transform del objeto "body"
@@ -27,7 +29,11 @@ public class mg_rewind : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        RewindState state = new RewindState { pos = transform.position, rot = mainBodyObj.transform.eulerAngles, scale = mainBodyObj.transform.localScale };//crea un nuevo elemento para contener la posición, rotación y escala actuales del objeto
+        RewindState state = new RewindState { pos = transform.position, rot = mainBodyObj.transform.eulerAngles, scale = mainBodyObj.transform.localScale, action = null };//crea un nuevo elemento para contener la posición, rotación y escala actuales del objeto
+        if (action != null) {
+            state.action = action;
+            action = null;
+        }
         rewindStates.Add(state);//añade el nuevo elemento a la lista
         if (timeLimit <= 0)
             rewindStates.RemoveAt(0);//borra el elemento más viejo de la lista en caso de que el tiempo límite se agote
@@ -41,10 +47,14 @@ public class mg_rewind : MonoBehaviour {
             if (newRewindStates.Count <= 0)//lo cancela en caso de que la lista se quede sin elementos
                 RewindStop();
         }
-        if (replaying) {
+        if (replaying) {//la misma lecera que rewind pero la revés, no seas pao
             rewindableShadowObj.transform.position = newReplayStates[newReplayStates.Count - 1].pos;
             rewindableShadowObj.transform.eulerAngles = newReplayStates[newReplayStates.Count - 1].rot;
             rewindableShadowObj.transform.localScale = newReplayStates[newReplayStates.Count - 1].scale;
+            if (newReplayStates[newReplayStates.Count - 1].action != null) {//esta lecera revisa si hay un string guardado en el elemento de la lista, luego lo separa en distintos strings usando el ";" y agarra el primer string para indicarle al objeto y su parent que ejecute cualquier código con ese nombre, además de pasar el string original como una variable para ser descifrada al otro lado
+                string[] tempString = newReplayStates[newReplayStates.Count - 1].action.Split(';');
+                rewindableShadowObj.SendMessageUpwards(tempString[0], newReplayStates[newReplayStates.Count - 1].action, SendMessageOptions.DontRequireReceiver);
+            }
             newReplayStates.RemoveAt(newReplayStates.Count - 1);
             if (newReplayStates.Count <= 0) {//lo cancela en caso de que la lista se quede sin elementos
                 replaying = false;
@@ -54,6 +64,8 @@ public class mg_rewind : MonoBehaviour {
         }
     }
     public void RewindStart () {
+        if (rewinding || replaying)
+            return;
         newRewindStates = new List<RewindState>(rewindStates);//copia la lista principal para no afectarla
         if (newRewindStates.Count <= 0)//lo cancela en caso de que la lista no tenga ningún elemento
             return;
@@ -66,7 +78,13 @@ public class mg_rewind : MonoBehaviour {
     }
     public void RewindStop () {
         rewinding = false;
-        if(newReplayStates.Count > 0)
+        if(newReplayStates.Count > 0)//ejecuta el Replay en caso de que SI exista una lista de Rewind
             replaying = true;
+    }
+
+    public void AddAction(string methodName, string variable) {//asigna la variable "action" con la información del evento para luego ser aplicada al siguiente elemento guardado en rewindStates
+        if(variable != null)
+            action = methodName + ";" + variable;
+        else action = methodName + ";null";
     }
 }
