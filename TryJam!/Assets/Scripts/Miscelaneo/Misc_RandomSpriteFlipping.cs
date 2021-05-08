aea752c109bf09d12f40d1eefd277ea4dbd4388b
Plumbio <felipe.b@live.cl>
2021-05-08 01:05:16 -0400

    Añadido Spine runtimes, el modelo Spine de la mona china, nuevo escenario y una prueba pa las sombras proyectadas en el suelo

---
+++ TryJam!/Assets/Scripts/Miscelaneo/Misc_RandomSpriteFlipping.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Misc_RandomSpriteFlipping : MonoBehaviour {
+
+    bool axis;
+    float timer;
+    public float speed = 0.2f;
+    public SpriteRenderer render;
+
+    private void Start () {
+        timer = speed;
+    }
+
+    void Update() {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            if (axis)
+                render.flipX = !render.flipX;
+            else render.flipY = !render.flipY;
+            axis = !axis;
+            timer = speed;
+        }
+    }
+}
