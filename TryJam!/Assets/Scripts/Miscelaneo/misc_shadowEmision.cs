fa1aefddd257a852904594c012cd29c691f925cb
Plumbio <felipe.b@live.cl>
2021-05-08 03:08:48 -0400

    emisión de sombras programada (existe en forma de prefab pa metérsela a cualquier tontera)

---
+++ TryJam!/Assets/Scripts/Miscelaneo/misc_shadowEmision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class misc_shadowEmision : MonoBehaviour{
+
+    public Transform target;
+
+    private void Start () {
+        transform.SetParent(null);
+    }
+
+    void Update() {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.rotation = target.rotation;
+    }
+}
