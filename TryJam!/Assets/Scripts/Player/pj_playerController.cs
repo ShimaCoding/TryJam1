f5cf1253eef8ab8654ccc33ae514861f37fd8e45
Plumbio <felipe.b@live.cl>
2021-05-08 06:21:30 -0400

    Nueva textura de plataforma, animaciones de mona china

--- TryJam!/Assets/Scripts/Player/pj_playerController.cs
+++ TryJam!/Assets/Scripts/Player/pj_playerController.cs
@@ -9,6 +9,10 @@ public class pj_playerController : MonoBehaviour
     public float moveSpeed = 7f;
     public Rigidbody rb;
     Vector3 movement;
+<<<<<<< Updated upstream
+=======
+    bool moving;
+>>>>>>> Stashed changes
     public float attackRange = 3f;
 
     private Vector3 mousePos;
@@ -28,7 +32,11 @@ public class pj_playerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+<<<<<<< Updated upstream
         //spineSkel.GetComponentInChildren<SkeletonAnimation>();
+=======
+        spineSkel.AnimationState.SetAnimation(0, "IDLE", true);
+>>>>>>> Stashed changes
     }
 
     void Update()
@@ -45,11 +53,27 @@ public class pj_playerController : MonoBehaviour
             Ray ray = new Ray(transform.position, (clickPosition - transform.position).normalized);
             if (Physics.Raycast(ray, out hit2, attackRange, entityLayer)) {
                 if (hit2.transform.CompareTag("Enemy")) {
+<<<<<<< Updated upstream
                     if (Input.GetMouseButtonDown(1))
                         hit2.transform.GetComponent<mg_rewind>().RewindStart();
                     if (Input.GetMouseButtonDown(0))
                         PushEnemy(hit2.transform.GetComponent<Rigidbody>());
                         
+=======
+                    if (Input.GetMouseButtonDown(1)) {
+                        hit2.transform.GetComponent<mg_rewind>().RewindStart();
+                        spineSkel.AnimationState.ClearTrack(2);
+                        spineSkel.AnimationState.SetAnimation(2, "Cast", false);
+                        spineSkel.AnimationState.AddAnimation(2, "Cast", false, 0);
+                        spineSkel.AnimationState.AddAnimation(2, "IDLE", true, 0);
+                    }
+                    if (Input.GetMouseButtonDown(0)) {
+                        PushEnemy(hit2.transform.GetComponent<Rigidbody>());
+                        spineSkel.AnimationState.ClearTrack(2);
+                        spineSkel.AnimationState.SetAnimation(2, "Push", false);
+                        spineSkel.AnimationState.AddAnimation(2, "IDLE", true, 0);
+                    }
+>>>>>>> Stashed changes
                 }
             }
         }
@@ -63,9 +87,18 @@ public class pj_playerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
 
+<<<<<<< Updated upstream
         if(moveDirection.magnitude != 0)
             lastMoveDir = moveDirection;
         //Debug.Log(lastMoveDir.normalized + "last dir");
+=======
+        if (moveDirection.magnitude != 0) {
+            lastMoveDir = moveDirection;
+            if (!moving)
+                MoveStart();
+        }
+        else MoveEnd();
+>>>>>>> Stashed changes
     }
 
     private void FixedUpdate()
@@ -97,18 +130,25 @@ public class pj_playerController : MonoBehaviour
     {
         Vector3 direction = (enemyRb.transform.position - transform.position).normalized * pushEnemyForce;
         enemyRb.AddForce(direction);
+<<<<<<< Updated upstream
         AudioManager.instance.Play("ForceStaff");
+=======
+>>>>>>> Stashed changes
     }
 
     private void HandleDash()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+<<<<<<< Updated upstream
         {
             transform.position += lastMoveDir * dashDistance;
             AudioManager.instance.Play("Teletransportation");
         }
 
 
+=======
+            transform.position += lastMoveDir * dashDistance;
+>>>>>>> Stashed changes
     }
 
     void FlipPlayer(bool facing)
@@ -121,4 +161,16 @@ public class pj_playerController : MonoBehaviour
         foreach (Collider col in cols)
             col.enabled = false;
     }
+<<<<<<< Updated upstream
+=======
+
+    void MoveStart () {
+        moving = true;
+        spineSkel.AnimationState.SetAnimation(3, "Walk", true);
+    }
+    void MoveEnd () {
+        moving = false;
+        spineSkel.AnimationState.ClearTrack(3);
+    }
+>>>>>>> Stashed changes
 }
